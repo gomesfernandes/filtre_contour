b7267ad21759843ca75ba9e86c6bc7132802c4d8
Gomes Fernandes Caty <caty.gomes-fernandes@turing.u-strasbg.fr>
2016-11-11 18:57:02 +0100

    Ajouter nouveau filtres

--- filtres_contour/View.java
+++ filtres_contour/View.java
@@ -9,14 +9,18 @@ import javax.swing.*;
 public class View extends JFrame{
  
 	private static final long serialVersionUID = 1L;
-	private JButton bouton = new JButton("Filtre de Sobel");
-  private JButton bouton2 = new JButton("Second Filtre");
-  private JPanel container = new JPanel();
-  private JLabel label = new JLabel();
-  //private Image image ;
+	private JButton bouton = new JButton("Sobel");
+	private JButton bouton2 = new JButton("Robert's Cross");
+	private JButton bouton3 = new JButton("Prewitt");
+	private JButton boutonFermer = new JButton("Fermer");
+	private JPanel container = new JPanel();
+	private JLabel label = new JLabel();
+    //private Image image ;
   
 	ImageOriginale i = new ImageOriginale("Valve_original.png");
 	SobelFilter f = new SobelFilter();
+	RobertsCrossFilter r = new RobertsCrossFilter();
+	PrewittFilter p = new PrewittFilter();
 
   //ImageIcon image = new ImageIcon();
   
@@ -32,10 +36,14 @@ public class View extends JFrame{
    
     bouton.addActionListener(new BoutonListener());
     bouton2.addActionListener(new Bouton2Listener());
+    bouton3.addActionListener(new Bouton3Listener());
+    boutonFermer.addActionListener(new BoutonFermerListener());
         
     JPanel south = new JPanel();
     south.add(bouton);
     south.add(bouton2);
+    south.add(bouton3);
+    south.add(boutonFermer);
     container.add(south, BorderLayout.SOUTH);
 
     /*label = new JLabel()
@@ -56,22 +64,34 @@ public class View extends JFrame{
        
   
   class BoutonListener implements ActionListener{
-    
     public void actionPerformed(ActionEvent arg0) {
-
 		i.applyFilter(f);
-		
 		i.save("test_sobel.PNG");
-
     	label.setIcon(new ImageIcon("test_sobel.PNG"));
-
     }
   }
       
  
   class Bouton2Listener implements ActionListener{
     public void actionPerformed(ActionEvent e) {
-   
+    	i.applyFilter(r);
+		i.save("test_sobel.PNG");
+    	label.setIcon(new ImageIcon("test_sobel.PNG"));
     }
-  }      
+  }     
+  
+  class Bouton3Listener implements ActionListener{
+    public void actionPerformed(ActionEvent e) {
+    	i.applyFilter(p);
+		i.save("test_sobel.PNG");
+    	label.setIcon(new ImageIcon("test_sobel.PNG"));
+    }
+  }  
+  
+  class BoutonFermerListener implements ActionListener{
+    public void actionPerformed(ActionEvent e) {
+    	System.exit(0);
+    }
+  }  
+  
 }
