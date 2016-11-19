cf7960e37239448586b637fd5240f79b86bc6745
mlamri <lamri.manal67@gmail.com>
2016-11-19 18:28:56 +0100

    ajout browser

--- filtres_contour/View.java
+++ filtres_contour/View.java
@@ -2,23 +2,26 @@ package filtres_contour;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
  
 public class View extends JFrame{
  
 	private static final long serialVersionUID = 1L;
 	private JButton bouton = new JButton("Filtre de Sobel");
-  private JButton bouton2 = new JButton("Second Filtre");
-  private JPanel container = new JPanel();
-  private JLabel label = new JLabel();
-  //private Image image ;
+	private JButton bouton2 = new JButton("Second Filtre");
+	private JButton bouton3 = new JButton("Ouvrir...");
+	private JPanel container = new JPanel();
+	private JLabel label = new JLabel();
+	JFileChooser filechooser= new JFileChooser();
   
-	ImageOriginale i = new ImageOriginale("Valve_original.png");
+	ImageOriginale i ;//= new ImageOriginale("test");
 	SobelFilter f = new SobelFilter();
-
-  //ImageIcon image = new ImageIcon();
   
   public View() throws IOException{
 	  
@@ -29,26 +32,23 @@ public class View extends JFrame{
  
     container.setBackground(Color.white);
     container.setLayout(new BorderLayout());
+    
+    bouton.setEnabled(false);
+    bouton2.setEnabled(false);
    
     bouton.addActionListener(new BoutonListener());
     bouton2.addActionListener(new Bouton2Listener());
+    bouton3.addActionListener(new Bouton3Listener());
         
     JPanel south = new JPanel();
     south.add(bouton);
     south.add(bouton2);
+    south.add(bouton3);
     container.add(south, BorderLayout.SOUTH);
 
-    /*label = new JLabel()
-    {
-        public void paintComponent(Graphics g)
-        {
-            super.paintComponent(g);
-            image.paintIcon(this, g, image.getIconWidth(), image.getIconHeight());
-        }
-    };*/
     
     label.setHorizontalAlignment(JLabel.CENTER);
-    label.setIcon(new ImageIcon("Valve_original.png"));
+    
     container.add(label, BorderLayout.CENTER);
     this.setContentPane(container);
     this.setVisible(true);
@@ -58,12 +58,13 @@ public class View extends JFrame{
   class BoutonListener implements ActionListener{
     
     public void actionPerformed(ActionEvent arg0) {
+    	
 
 		i.applyFilter(f);
 		
-		i.save("test_sobel.PNG");
-
-    	label.setIcon(new ImageIcon("test_sobel.PNG"));
+		i.save("test_sobel_teeeeest.PNG");
+		//System.out.println(filechooser.getSelectedFile().getAbsolutePath());
+    	label.setIcon(new ImageIcon("test_sobel_teeeeest.PNG"));
 
     }
   }
@@ -73,5 +74,34 @@ public class View extends JFrame{
     public void actionPerformed(ActionEvent e) {
    
     }
-  }      
+  }
+  
+  class Bouton3Listener implements ActionListener{
+	    public void actionPerformed(ActionEvent ev) { 	
+	    	
+	    	filechooser.setDialogTitle("Choisissez un fichier..");
+	        filechooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+	        int returnval=filechooser.showOpenDialog(null);
+	        if(returnval==JFileChooser.APPROVE_OPTION)
+	        {
+	            File file = filechooser.getSelectedFile();
+	            BufferedImage bi;
+	            try
+	            {   
+	                bi=ImageIO.read(file);
+	                label.setIcon(new ImageIcon(bi));
+	            }
+	            catch(IOException e)
+	            {
+
+	            }
+	            pack();
+	        }
+	        
+	        i = new ImageOriginale(filechooser.getSelectedFile().getAbsolutePath());
+	        System.out.println(filechooser.getSelectedFile().getAbsolutePath());
+	        bouton.setEnabled(true);
+	        bouton2.setEnabled(true);
+	    }
+  }
 }
